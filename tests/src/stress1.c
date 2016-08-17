a11dc02f01b759a895234d794a12739c7207a941
James Litton <litton@cs.umd.edu>
2016-08-17 07:16:54 -0400

    add some capability for this to time switch time in windows

--- tests/src/stress1.c
+++ tests/src/stress1.c
@@ -9,8 +9,8 @@
 
 #include "lwc.h"
 
-#define N 100
-
+#define N 12
+#define WIND 1
 int main() {
 
 	register_t *mbuf = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
@@ -40,6 +40,11 @@ int main() {
 	specs[0].flags = LWC_RESOURCE_FILES | LWC_RESOURCE_SHARE;
 	specs[0].sub.descriptors.from = specs[0].sub.descriptors.to = -1;
 
+	time_t start = time(NULL);
+	time_t last = start;
+	size_t switches = 0;
+
+
 	int parent_snap = lwcgetlwc();
 
 	if (parent_snap < 0) {
@@ -59,7 +64,7 @@ int main() {
 			perror("Can't snap");
 			abort();
 		} else {
-			printf("Child %d doing something\n", lwcgetlwc());
+			//printf("Child %d doing something\n", lwcgetlwc());
 			lwcdiscardswitch(parent_snap, NULL, 0);
 			perror("child could not discard switch");
 			abort();
@@ -67,9 +72,22 @@ int main() {
 	}
 
 	int cur = 0;
-	for(;sbuf[0] > 0 && switches_remaining > 0;--switches_remaining) {
+	for(;;) {
+		//for(;sbuf[0] > 0 && switches_remaining > 0;--switches_remaining) {
+		switches++;
+
+		time_t curtm = time(NULL);
+		if (curtm - last >= WIND) {
+			printf("Running %ld seconds with %f switches per %d second window\n",
+			       curtm - start, (1.0*switches) / (1.0 * WIND), WIND);
+			switches = 0;
+			last = curtm;
+		}
+
+/*
 		printf("parent doing switch to sbuf[%d]=%ld, remaining = %d\n",
 		       cur, sbuf[cur], switches_remaining);
+*/
 		lwcsuspendswitch(sbuf[cur], NULL, 0, NULL, NULL, 0);
 		cur = (cur + 1) % N;
 	}
